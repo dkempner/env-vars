6a51461363a28225d88cc5aae81de77866952b90
Daniel Kempner <dkempner@lyft.com>
2020-12-03 13:27:13 -0800

    env vars

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+const { A, B, C } = process.env;
+
+console.log({
+  A,
+  B,
+  C,
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
